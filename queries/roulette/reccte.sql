39c67ed5fd3112644067cb6656a17fca582a941f
James McNeill <jpmmcneill@gmail.com>
2024-01-27 21:59:43 +0000

    update queries

--- queries/roulette/reccte.sql
+++ queries/roulette/reccte.sql
@@ -11,15 +11,17 @@ with recursive casino as (
         casino.sample,
         case
             when casino.value = 0 then 0
-        else casino.value + (case when floor(random() * (37)) >= 19 then 10 else -10 end)
+        else casino.value + (
+            case
+                when floor(random() * (37)) >= 19 then 10
+                else -10
+            end
+        )
         end as value,
         casino.turn + 1 as turn
     from casino
-    where value >= 0 and casino.turn < 10000
+    where value >= 0 and casino.turn < 100
 )
 
 select * from casino
--- select turn, avg(value) as avg_value, var_samp(value) as var_samp_value
--- from casino
--- group by all
--- order by turn asc
+;
--- queries/roulette/windowdense.sql
+++ queries/roulette/windowdense.sql
@@ -4,9 +4,14 @@ select
     samples.generate_series as sample,
     turns.generate_series as turn,
     1000 + sum(
-        case when floor(random() * (37)) >= 19 then 10 else -10 end
+        -- this doesn't account for negative values
+        -- but that doesn't matter for speed purposes
+        -- as that would be one extra window function!
+        case
+            when floor(random() * (37)) >= 19
+            then 10 else -10 end
     ) over (
         partition by sample order by turn
     ) as value
 from generate_series(1, 1000) as samples
-cross join generate_series(1, 10000) as turns
+cross join generate_series(1, 100) as turns
